15fee2a9ef27047859e4c2d636ad626551b08892
Lukasz Malinowski <lukasz.malinowski@uniteam.pl>
2020-05-18 12:25:51 +0200

    add about quiz activity

---
+++ app/src/main/java/pl/org/ponton/pontonquizapp/activities/menu/QuizActivity.java
@@ -0,0 +1,16 @@
+package pl.org.ponton.pontonquizapp.activities.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.org.ponton.pontonquizapp.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
